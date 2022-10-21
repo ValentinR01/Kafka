87c3db85d60e6df12b3df9928a7ecb0b7b95dd45
ValentinR01 <valentin.rejaunier@hetic.net>
2022-10-22 01:28:14 +0200

    Starting Flask

--- microservices/returnApi/app.py
+++ microservices/returnApi/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+@app.route('/ping', methods = ['GET'])
+def ping():
+    return 'pong'
+
+@app.route('/return/<int:id>', methods = ['GET'])
+def showReturnById(id):
+    return str(id)
+
+@app.route('/return/<int:id>', methods = ['PATCH'])
+def updateReturnById(id):
+    return 'patch'
+
+#@app.route('/return/create')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=80)
+
